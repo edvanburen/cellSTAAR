1bd76c8b99c175c0bd6a803b7a532cf89bb5205b
Eric Van Buren <edvanburen@gmail.com>
2023-10-16 14:24:05 -0400

    test

--- R/create_ct_aPCs.R
+++ R/create_ct_aPCs.R
@@ -86,15 +86,13 @@ create_ct_aPCs<-function(gds.path
         region_file<-inner_join(region_file,new,by="position")
       }
     }
-    region_file<-region_file%>%mutate(score=rowMeans(across(contains(ct_name))))%>%
+    t0<-region_file%>%mutate(score=rowMeans(across(contains(ct_name))))%>%
       dplyr::select(.data$position,.data$score)%>%ungroup()
-    colnames(region_file)[colnames(region_file)=="score"]<-paste0("score_",ct_name)
+    colnames(t0)[colnames(t0)=="score"]<-paste0("score_",ct_name)
+    assign(t0,region_file)
   }else{
     assign(t0,process_bw_aPCs(path=atac_file_path,ct=ct_name,chr_filter = paste0("chr",chr)))
   }
-
-
-    assign(t0,region_file)
 
     t0<-t0%>%dplyr::slice(rep(1:nrow(t0),table(variant_pos)))
 
