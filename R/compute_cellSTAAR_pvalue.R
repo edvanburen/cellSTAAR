fc9398a09c255c9cd8644136728c85de965ca0ea
Eric Van Buren <edvanburen@gmail.com>
2024-12-06 13:45:49 -0500

    Allow exponential decay in distance linking weights

--- R/compute_cellSTAAR_pvalue.R
+++ R/compute_cellSTAAR_pvalue.R
@@ -2,13 +2,14 @@
 ##' @param data_obj Data frame of all results from the \code{run_cellSTAAR} function. By controlling the \code{grouping_vars} parameter, multiple phenotypes, cell types, linking types, and genes can be input simultaneously.
 ##' @param grouping_vars Set of variables (Other than "link_type"!) that uniquely identify a row in \code{data_obj}.
 ##' @param use_conditional_p_if_exist Should the function compute the cellSTAAR omnibus p-value using conditional p-values when they exist? Defaults to FALSE.
+##' @param exp_decay_dist_val Numeric value controlling the degree of exponential decay applied to the distance linking approaches for enhancer regions (pELS and dELS). If 0, all distance intervals are weighted equally. The default value of .0001 means that the six intervals 0_1, 1_50000, 50000_100000, 100000_150000, 150000_200000, and 200000_250000 are weighted 41.4%, 25.1%, 15.2%, 9.2%, 5.6%, and 3.4%, respectively.
 ##' @export compute_cellSTAAR_pvalue
 
-compute_cellSTAAR_pvalue<-function(data_obj,grouping_vars=c("gene","chr","phenotype","element_class","ct_name"),use_conditional_p_if_exist=FALSE){
+compute_cellSTAAR_pvalue<-function(data_obj,grouping_vars=c("gene","chr","phenotype","element_class","ct_name"),use_conditional_p_if_exist=FALSE,exp_decay_dist_val=-.00001){
   CCT_removeNA <- function(pvals, weights=NULL){
+    if(!is.null(weights)){weights<-weights[!is.na(pvals)]}
     pvals<-pvals[!is.na(pvals)]
     if(length(pvals)==0){return(NA)}
-    if(!is.null(weights)){weights<-weights[!is.na(pvals)]}
     #### check if there is NA
     if(sum(is.na(pvals)) > 0){
       stop("Cannot have NAs in the p-values!")
@@ -78,8 +79,14 @@ compute_cellSTAAR_pvalue<-function(data_obj,grouping_vars=c("gene","chr","phenot
     data_obj$num_rare_var[index]<-data_obj$num_rare_SNV_cond[index]
   }
 
-  browser()
-  t0<-data_obj%>%filter(grepl("dist",.data$link_type))%>%group_by(across(all_of(grouping_vars)))%>%mutate(CCT_pval=CCT_removeNA(.data$pvalue),num_rare_var=round(mean(num_rare_var,na.rm=TRUE),0))%>%dplyr::select(all_of(grouping_vars),CCT_pval,num_rare_var)%>%distinct()%>%ungroup()
+  #browser()
+  t0<-data_obj%>%filter(grepl("dist",.data$link_type))%>%
+    mutate(dist_end =as.numeric(sub(".*_(\\d+)$", "\\1", link_type)))%>%
+    mutate(link_weight=exp(-1*exp_decay_dist_val*dist_end))
+
+
+
+  t0<-t0%>%group_by(across(all_of(grouping_vars)))%>%mutate(CCT_pval=CCT_removeNA(.data$pvalue,.data$link_weight),num_rare_var=round(mean(num_rare_var,na.rm=TRUE),0))%>%dplyr::select(all_of(grouping_vars),CCT_pval,num_rare_var)%>%distinct()%>%ungroup()
 
   colnames(t0)[colnames(t0)=="CCT_pval"]<-"pvalue"
 
